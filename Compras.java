abe9aae2e892af6695d48301089006aadd761d5e
juliocsantana <gollosantana5@gmail.com>
2016-09-29 17:49:00 -0700

    Create Compras.java

---
+++ Compras.java
@@ -0,0 +1,83 @@
+
+public class Compras {
+	public static void main(String[] args) {
+		
+		Auto car1= new Usado();
+		car1.setModelo("Toyota");
+		car1.setMarca("Celica");
+		car1.setColor("Blanco");
+		car1.setPrecio(100000);
+		
+		Auto car2= new Usado();
+		car2.setModelo("Toyota");
+		car2.setMarca("Cambry");
+		car2.setColor("Azul");
+		car2.setPrecio(100000);
+		
+		Auto car3= new Usado();
+		car3.setModelo("Toyota");
+		car3.setMarca("Corolla");
+		car3.setColor("Cafe");
+		car3.setPrecio(100000);
+		
+		Auto car4= new Usado();
+		car4.setModelo("Nissan");
+		car4.setMarca("Sentra");
+		car4.setColor("Rojo");
+		car4.setPrecio(100000);
+		
+		Auto car5= new Usado();
+		car5.setModelo("Nissan");
+		car5.setMarca("Altima");
+		car5.setColor("Blanco");
+		car5.setPrecio(100000);
+		
+		Auto car6= new Usado();
+		car6.setModelo("Toyota");
+		car6.setMarca("Celica");
+		car6.setColor("Blanco");
+		car6.setPrecio(100000);
+		
+		Auto car7= new Usado();
+		car7.setModelo("Nissan");
+		car7.setMarca("Maxima");
+		car7.setColor("Negro");
+		car7.setPrecio(100000);
+		
+		Auto car8= new Usado();
+		car8.setModelo("Nissan");
+		car8.setMarca("Versa");
+		car8.setColor("Rojo");
+		car8.setPrecio(100000);
+		
+		Auto car9= new Usado();
+		car9.setModelo("Ford");
+		car9.setMarca("Focus");
+		car9.setColor("Azul");
+		car9.setPrecio(100000);
+		
+		Auto car10= new Usado();
+		car10.setModelo("Ford");
+		car10.setMarca("Fusion");
+		car10.setColor("Blanco");
+		car10.setPrecio(100000);
+		
+		
+		System.out.println("Modelo: "+car1.getModelo()+ " Marca: "+car1.getMarca()+" Color: "+car1.getColor()+" Precio: "+  car1.calcular());
+		System.out.println("Modelo: "+car2.getModelo()+ " Marca: "+car2.getMarca()+" Color: "+car2.getColor()+" Precio: "+  car2.calcular());
+		System.out.println("Modelo: "+car3.getModelo()+ " Marca: "+car3.getMarca()+" Color: "+car3.getColor()+" Precio: "+  car3.calcular());
+		System.out.println("Modelo: "+car4.getModelo()+ " Marca: "+car4.getMarca()+" Color: "+car4.getColor()+" Precio: "+  car4.calcular());
+		System.out.println("Modelo: "+car5.getModelo()+ " Marca: "+car5.getMarca()+" Color: "+car5.getColor()+" Precio: "+  car5.calcular());
+		System.out.println("Modelo: "+car6.getModelo()+ " Marca: "+car6.getMarca()+" Color: "+car6.getColor()+" Precio: "+  car6.calcular());
+		System.out.println("Modelo: "+car7.getModelo()+ " Marca: "+car7.getMarca()+" Color: "+car7.getColor()+" Precio: "+  car7.calcular());
+		System.out.println("Modelo: "+car8.getModelo()+ " Marca: "+car8.getMarca()+" Color: "+car8.getColor()+" Precio: "+  car8.calcular());
+		System.out.println("Modelo: "+car9.getModelo()+ " Marca: "+car9.getMarca()+" Color: "+car9.getColor()+" Precio: "+  car9.calcular());
+		System.out.println("Modelo: "+car10.getModelo()+ " Marca: "+car10.getMarca()+" Color: "+car10.getColor()+" Precio: "+  car10.calcular());
+		
+		
+		
+	    
+	   
+		
+	}
+}
